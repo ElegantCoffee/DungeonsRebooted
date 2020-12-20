1874e38d4c3f9d17daf5026559a8dd2905cfb255
elegantcoffee <elegantcoffee@users.noreply.github.com>
2020-12-20 22:46:06 +0100

    Initial commit

---
+++ src/main/java/com/elegantcoffee/dungeonsrebooted/DungeonsRebooted.java
@@ -0,0 +1,20 @@
+package com.elegantcoffee.dungeonsrebooted;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = DungeonsRebooted.MODID, version = DungeonsRebooted.VERSION)
+public class DungeonsRebooted
+{
+    public static final String MODID = "examplemod";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
